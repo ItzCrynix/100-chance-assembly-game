b429db511531037fb398b6863dd20879492f4f8d
ItzCrynix <cainanloyola376@gmail.com>
2025-12-03 18:20:46 -0300

    new movement function

--- semchance.asm
+++ semchance.asm
@@ -7,7 +7,9 @@ jmp main
 ; Player related
 
 playerPos: var #1
-	static playerPos, #0
+	static playerPos, #600
+playerChar: var #1
+	static playerChar, #30
 
 maxMoves: var #1
 	static maxMoves, #100
@@ -44,12 +46,124 @@ repeatPos: var #1
 ; Main function
 ;
 
+; r0 -> stores any position
+; r1 -> stores any character
+
 main:
-	halt
+	call move
+	call printPlayer
+	jmp main
 
 ;
 ; Functions
 ;
+
+move:
+	; Verifies if a key was pressed
+	inchar r1
+	loadn r2, #255
+	cmp r1, r2
+	jeq move
+	
+	; Verifies whether the pressed key was WASD
+	loadn r2, #'w'
+	cmp r1, r2
+	jeq move_up
+
+	loadn r2, #'s'
+	cmp r1, r2
+	jeq move_down
+
+	loadn r2, #'a'
+	cmp r1, r2
+	jeq move_left
+
+	loadn r2, #'d'
+	cmp r1, r2
+	jeq move_right
+
+	move_leave:
+		rts
+
+	move_up:
+		load r0, playerPos
+	
+		; Checking if player is at the top (pos < 40)
+		loadn r2, #40
+		cmp r0, r2
+		jle move_leave
+
+		; New position that the player would have, needs to be checked
+		call printClearPlayer
+		sub r0, r0, r2
+		call move_checkCollision
+
+		; New position gets saved.
+		store playerPos, r0
+
+		rts
+
+	move_down:
+		load r0, playerPos
+	
+		; Checking if player is at the bottom (pos > 1159)
+		loadn r2, #1159
+		cmp r0, r2
+		jgr move_leave
+
+		; New position that the player would have, needs to be checked
+		call printClearPlayer
+		loadn r2, #40
+		add r0, r0, r2
+		call move_checkCollision
+
+		; New position gets saved.
+		store playerPos, r0
+
+		rts
+
+	move_left:
+		load r0, playerPos
+	
+		; Checking if player is at the left border
+		loadn r2, #40
+		mod r2, r0, r2
+		loadn r3, #0
+		cmp r2, r3
+		jeq move_leave
+
+		; New position that the player would have, needs to be checked
+		call printClearPlayer
+		dec r0
+		call move_checkCollision
+
+		; New position gets saved.
+		store playerPos, r0
+
+		rts
+
+	move_right:
+		load r0, playerPos
+	
+		; Checking if player is at the right border
+		loadn r2, #40
+		mod r2, r0, r2
+		loadn r3, #39
+		cmp r2, r3
+		jeq move_leave
+
+		; New position that the player would have, needs to be checked
+		call printClearPlayer
+		inc r0
+		call move_checkCollision
+
+		; New position gets saved.
+		store playerPos, r0
+
+		rts
+
+	move_checkCollision:
+		rts
 
 ; prints a pre-made screen
 ; r1 = address of the first char
@@ -85,34 +199,34 @@ printScreen:
 	pop r1
 	rts
 
-printCurrentScreen:
-	push r1
-	push r3
+; printCurrentScreen:
+; 	push r1
+; 	push r3
 
-	loadn r3, #currentMap
-	loadi r3, r3
+; 	loadn r3, #currentMap
+; 	loadi r3, r3
 
-	loadn r1, #0
-	cmp r3, r1
-	jeq printLab1
+; 	loadn r1, #0
+; 	cmp r3, r1
+; 	jeq printLab1
 
-	loadn r1, #1
-	cmp r3, r1
-	jeq printLab2
+; 	loadn r1, #1
+; 	cmp r3, r1
+; 	jeq printLab2
 
-	printLab1:
-		loadn r1, #lab1
-		jmp printCurrentScreenLeave
+; 	printLab1:
+; 		loadn r1, #lab1
+; 		jmp printCurrentScreenLeave
 
-	printLab2:
-		loadn r1, #lab2
-		jmp printCurrentScreenLeave
+; 	printLab2:
+; 		loadn r1, #lab2
+; 		jmp printCurrentScreenLeave
 
-	printCurrentScreenLeave:
-		call printScreen
-		call printBaseScreen
-		pop r1
-		rts
+; 	printCurrentScreenLeave:
+; 		call printScreen
+; 		call printBaseScreen
+; 		pop r1
+; 		rts
 
 printString:
 	push r2
@@ -137,3 +251,24 @@ printString:
 		pop r3
 		pop r2
 		rts
+
+printPlayer:
+	push r0
+	push r1
+
+	load r0, playerChar
+	load r1, playerPos
+	outchar r0, r1
+
+	pop r1
+	pop r0
+	rts
+
+printClearPlayer:
+	push r1
+
+	loadn r1, #0
+	outchar r1, r0
+
+	pop r1
+	rts
